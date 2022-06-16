56af2b3f83100258c046dc9923b16fe707160959
Tianle Chen <tianlechen@gmail.com>
2022-06-16 18:18:05 -0400

    maxResultSize

--- spark_utils.py
+++ spark_utils.py
@@ -7,6 +7,7 @@ def get_spark_session(num_cores: int = 8, gigs_ram: int = 16) -> SparkSession:
         .builder
         .master(f'local[{num_cores}]')
         .config('spark.driver.memory', f'{gigs_ram}g')
+        .config('spark.driver.maxResultSize', f'{gigs_ram}g')
         # enables arrow-related features if using JDK 11
         # https://spark.apache.org/docs/latest/api/python/getting_started/install.html
         .config('Dio.netty.tryReflectionSetAccessible', 'true')
