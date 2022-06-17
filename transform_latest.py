201d0097031fa14e253184dd97aef3736b2f874e
Tianle Chen <tianlechen@gmail.com>
2022-06-17 11:14:47 -0400

    validations

--- transform_latest.py
+++ transform_latest.py
@@ -42,4 +42,26 @@ if __name__ == '__main__':
         )
         print(f'Wrote to {out_p}')
 
+    # validations
+    test_data = spark.read.parquet(
+        'data_transformed/amex-default-prediction/test_data_latest')
+    train_data = spark.read.parquet(
+        'data_transformed/amex-default-prediction/train_data_latest')
+    train_labels = spark.read.parquet(
+        'data/amex-default-prediction/train_labels')
+    sample_submission = spark.read.parquet(
+        'data/amex-default-prediction/sample_submission')
+
+    assert train_data.count() == train_data.select('customer_ID').distinct().count()
+    assert train_labels.count() == train_labels.select(
+        'customer_ID').distinct().count()
+    assert train_data.count() == train_data.join(
+        train_labels, on='customer_ID', how='inner').count()
+
+    assert test_data.count() == test_data.select('customer_ID').distinct().count()
+    assert sample_submission.count() == sample_submission.select(
+        'customer_ID').distinct().count()
+    assert test_data.count() == test_data.join(
+        sample_submission, on='customer_ID', how='inner').count()
+
     spark.stop()
