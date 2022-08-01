bcad103a49d6911693b1f569c2bfbc3e42aded94
Tianle Chen <tianlechen@gmail.com>
2022-08-01 19:36:21 -0400

    BatchedLGBMClassifier

--- batched.py
+++ batched.py
@@ -1,9 +1,14 @@
 from math import ceil
 from typing import List
 
+import pandas as pd
 import pyspark.sql.functions as F
-from pyspark.sql import DataFrame
+from lightgbm import LGBMClassifier
 from pyspark import StorageLevel
+from pyspark.sql import DataFrame
+
+from format_data import PREDICTION_VARIABLE
+from getxy import GetXY
 
 __BATCH_INDEX_COLUMN__ = '__BATCH_INDEX_COLUMN__'
 RANDOM_SEED = 20220801
@@ -36,3 +41,35 @@ class Batched:
 
     def fit_transform(self, df: DataFrame):
         return self.fit(df=df).transform(df=df)
+
+
+class BatchedLGBMClassifier:
+    def __init__(self, lgb_params: dict, getxy: GetXY):
+        self.lgb_params = lgb_params
+        self.getxy = getxy
+        self.model = None
+
+    def fit(self, dfs: List[DataFrame]):
+        for i, df in enumerate(dfs):
+            print(f'Fitting {i}/{len(dfs)} with {df.count()} rows')
+            X, y = self.getxy.transform(df=df)
+            fit_params = {}
+            if self.model is not None:
+                fit_params.update({'init_model': self.model})
+            self.model = LGBMClassifier(
+                **self.lgb_params).fit(X=X, y=y, **fit_params)
+        return self
+
+    def predict(self, dfs: List[DataFrame], id_variables: List[str]):
+        pred_outputs = []
+        for i, df in enumerate(dfs):
+            print(f'Predicting {i}/{len(dfs)} with {df.count()} rows')
+            X, y = getxy.transform(df=df)
+            pred_outputs.append((
+                df.select(id_variables).toPandas(),
+                self.model.predict_proba(X=X)
+            ))
+        ids, preds = zip(*pred_outputs)
+        pred_df = pd.concat(ids, axis=0)
+        pred_df[PREDICTION_VARIABLE] = np.concatenate(preds, axis=0)[:, 1]
+        return pred_df
