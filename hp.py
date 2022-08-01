642a50756976248b8dda264379e33d429a3a2142
Tianle Chen <tianlechen@gmail.com>
2022-07-31 21:27:55 -0400

    split em out

---
+++ hp.py
@@ -0,0 +1,102 @@
+import hyperopt
+from evaluation import feval_amex, feval_amex_gini, feval_amex_top4
+from sklearn import model_selection
+import pandas as pd
+from lightgbm import LGBMClassifier
+import numpy as np
+import mlflow
+from typing import List
+
+RANDOM_STATE = 20220731
+
+
+def get_cv_hp_metrics(
+    X_train: pd.DataFrame,
+    y_train: np.array,
+    X_test: pd.DataFrame,
+    y_test: np.array,
+    categorical_feature: List[str],
+    lgb_params: dict,
+) -> dict:
+    with mlflow.start_run(nested=True) as run:
+        mlflow.log_params(params=lgb_params)
+        model = LGBMClassifier(**lgb_params)
+        y_train_pred = model_selection.cross_val_predict(
+            estimator=model,
+            X=X_train,
+            y=y_train,
+            fit_params={'categorical_feature': categorical_feature},
+            method='predict_proba'
+        )[:, 1]
+
+        mlflow.lightgbm.autolog()
+        model = LGBMClassifier(**lgb_params)
+        model.fit(
+            X=X_test,
+            y=y_test,
+            categorical_feature=categorical_feature
+        )
+        y_test_pred = model.predict_proba(X=X_test)[:, 1]
+
+        metrics = {
+            # returns (metric_name, metric_value, higher_is_better)
+            'train_feval_amex': feval_amex(y_true=y_train, y_pred=y_train_pred)[1],
+            'train_feval_amex_gini': feval_amex_gini(y_true=y_train, y_pred=y_train_pred)[1],
+            'train_feval_amex_top4': feval_amex_top4(y_true=y_train, y_pred=y_train_pred)[1],
+            'test_feval_amex': feval_amex(y_true=y_test, y_pred=y_test_pred)[1],
+            'test_feval_amex_gini': feval_amex_gini(y_true=y_test, y_pred=y_test_pred)[1],
+            'test_feval_amex_top4': feval_amex_top4(y_true=y_test, y_pred=y_test_pred)[1],
+        }
+        mlflow.log_metrics(metrics=metrics)
+
+    return metrics
+
+
+def build_train_objective(
+    X_train: pd.DataFrame,
+    y_train: np.array,
+    X_test: pd.DataFrame,
+    y_test: np.array,
+    categorical_feature: List[str],
+    metric_name: str = 'test_feval_amex',
+    higher_is_better: bool = True,
+):
+    def obj_fn(lgb_params):
+        metrics = get_cv_hp_metrics(
+            X_train=X_train,
+            y_train=y_train,
+            X_test=X_test,
+            y_test=y_test,
+            categorical_feature=categorical_feature,
+            lgb_params=lgb_params,
+        )
+        return {
+            'status': hyperopt.STATUS_OK,
+            'loss': metrics[metric_name] * -1 if higher_is_better else 1.,
+        }
+    return obj_fn
+
+
+def find_best_run(
+    run: mlflow.entities.Run,
+    metric_name: str = 'test_feval_amex',
+    higher_is_better: bool = True,
+):
+    client = mlflow.tracking.MlflowClient()
+    nested_runs = client.search_runs(
+        [run.info.experiment_id],
+        "tags.mlflow.parentRunId = '{run_id}' ".format(run_id=run.info.run_id)
+    )
+    best_run = min(
+        nested_runs,
+        key=lambda run: run.data.metrics[metric_name] * -
+        1. if higher_is_better else 1.
+    )
+    mlflow.set_tag("best_run", best_run.info.run_id)
+    mlflow.log_metric(f"best_{metric_name}",
+                      best_run.data.metrics[metric_name])
+    print(
+        f'best run id: {best_run.info.run_id} over {len(nested_runs)} runs '
+        f'achieved best {metric_name} of {best_run.data.metrics[metric_name]} '
+    )
+    return best_run
---
+++ v3_hp.py
@@ -0,0 +1,92 @@
+from hp import build_train_objective, find_best_run
+import mlflow
+import hyperopt
+from hyperopt.pyll.base import scope
+from sklearn.model_selection import train_test_split
+import numpy as np
+from format_data import TARGET_VARIABLE, DATE_VARIABLES, ID_VARIABLES
+from encoder import CategoricalToIntegerEncoders
+from format_data import CATEGORICAL_VARIABLES
+from spark_utils import get_spark_session
+from hyperopt import Trials
+
+spark = get_spark_session()
+# run transform_latest.py if this don't exist
+test_data = spark.read.parquet(
+    'data_transformed/amex-default-prediction/test_data_latest')
+train_data = spark.read.parquet(
+    'data_transformed/amex-default-prediction/train_data_latest')
+# run format_data.py if these don't exist
+train_labels = spark.read.parquet('data/amex-default-prediction/train_labels')
+sample_submission = spark.read.parquet(
+    'data/amex-default-prediction/sample_submission')
+
+
+encs = CategoricalToIntegerEncoders(
+    columns=CATEGORICAL_VARIABLES).fit(train_data)
+
+train_pdf = train_data.join(train_labels, on='customer_ID', how='inner')
+train_pdf = encs.transform(spark=spark, df=train_pdf).toPandas()
+
+test_pdf = encs.transform(spark=spark, df=test_data).toPandas()
+
+
+non_feature_columns = [
+    TARGET_VARIABLE,
+    *ID_VARIABLES,
+    *DATE_VARIABLES.keys(),
+]
+feature_columns = [
+    c for c in train_pdf.columns if c not in non_feature_columns]
+print(
+    f'len(feature_columns): {len(feature_columns)}\n',
+    ', '.join(feature_columns)
+)
+
+
+X_fit = train_pdf[feature_columns].reset_index(drop=True)
+X_test = test_pdf[feature_columns].reset_index(drop=True)
+print(X_fit.shape, X_test.shape)
+
+y_fit = np.array(train_pdf[TARGET_VARIABLE])
+print(np.unique(y_fit, return_counts=True))
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_fit, y_fit)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+
+MAX_EVALS = 5
+
+space = {
+    'class_weight': {
+        0.: 1.,
+        1.: hyperopt.hp.uniform('class_weight', 0., 10.)
+    },
+    # 'subsample': hyperopt.hp.uniform('subsample', 0.05, 1.0),
+    # The parameters below are cast to int using the scope.int() wrapper
+    # 'num_iterations': scope.int(hyperopt.hp.quniform('num_iterations', 10, 200, 1)),
+    # 'num_leaves': scope.int(hyperopt.hp.quniform('num_leaves', 20, 50, 1))
+}
+
+
+with mlflow.start_run(nested=False) as run:
+    print(
+        f'run_id: {run.info.run_id} '
+        f'experiment_id: {run.info.experiment_id} '
+    )
+    train_objective = build_train_objective(
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+        categorical_feature=encs.columns_encoded,
+    )
+    hyperopt.fmin(
+        fn=train_objective,
+        space=space,
+        algo=hyperopt.tpe.suggest,
+        max_evals=MAX_EVALS,
+        trials=Trials()
+    )
+    find_best_run(run)
