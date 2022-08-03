796b63ec58860e5b37ee11a4f86bf251262fe39a
Tianle Chen <tianlechen@gmail.com>
2022-08-03 08:57:51 -0400

    better defaults

--- spark_utils.py
+++ spark_utils.py
@@ -1,23 +1,23 @@
+from typing import Optional
+
+import psutil
 from pyspark.sql import SparkSession
 
 
-def get_spark_session(num_cores: int = 8, gigs_ram: int = 16) -> SparkSession:
+def get_spark_session(num_cores: Optional[int] = None, memory: Optional[int] = None) -> SparkSession:
+    num_cores = '*' if num_cores is None else num_cores
+    memory = psutil.virtual_memory().total if memory is None else memory
+
     return (
         SparkSession
         .builder
         .master(f'local[{num_cores}]')
-        .config('spark.driver.memory', f'{gigs_ram}g')
-        .config('spark.driver.maxResultSize', f'{gigs_ram}g')
+        .config('spark.driver.memory', str(memory))
+        .config('spark.driver.maxResultSize', '0')
         # enables arrow-related features if using JDK 11
         # https://spark.apache.org/docs/latest/api/python/getting_started/install.html
         .config('Dio.netty.tryReflectionSetAccessible', 'true')
         .config('spark.sql.execution.arrow.pyspark.enabled', 'true')
-        # compression
-        .config('spark.shuffle.compress', 'true')
-        .config('spark.shuffle.spill.compress', 'true')
-        .config('spark.broadcast.compress', 'true')
-        .config('spark.checkpoint.compress', 'true')
-        .config('spark.rdd.compress', 'true')
         .getOrCreate()
     )
 
